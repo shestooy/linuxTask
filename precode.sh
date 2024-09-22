bced184817d3d58f258cdfcd56b1c18fb9dd102b
shestooy <km.km.2021@inbox.ru>
2024-09-22 21:33:00 +0300

    fix

--- precode.sh
+++ precode.sh
@@ -22,13 +22,13 @@ chmod 764 dir2/hello.sh
 ls dir2 > dir2/list.txt
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
 cp -r dir2/* dir3/dir4/
-# записываем в task/dir1/summary.txt список файловс расширением *.txt
+# записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
 find . -type f -name "*.txt" > dir1/summary.txt
 # дописываем в task/dir1/summary.txt содержимое task/dir2/list.txt
 cat dir2/list.txt >> dir1/summary.txt
 # определяем переменную окружения NAME со значением "Всем студентам"
-NAME="Всем студентам"
+export NAME="Всем студентам"
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
 ./dir2/hello.sh "$NAME" >> dir1/summary.txt
@@ -40,6 +40,6 @@ cat 'Практическое_задание'
 # и затем отсортировываем их
 grep 'dir' 'Практическое_задание' | sort
 # меняем текущую директорию на родительскую для task
-cd .. || exit 1
+cd ..
 # удаляем директорию task со всем содержимым
 rm -rf task
